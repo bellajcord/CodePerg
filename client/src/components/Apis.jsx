45fe199d86aeb2d786dcca74d09f929fdde55acf
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-03-12 16:35:08 -0400

    created add to form for api page

--- client/src/components/Apis.jsx
+++ client/src/components/Apis.jsx
@@ -11,19 +11,47 @@ export default class Apis extends Component {
     this.state = {
       apiVideos: [],
       searchField: '',
+      
     };
   }
 
-  componentDidMount = () => {
+  updatePage = () => {
     axios.get('/api/apivideo').then(res => {
         this.setState({apiVideos: res.data})
     })
   }
 
+  componentDidMount(){
+    this.updatePage();
+  };
+
+
   handleChange = (e) => {
     this.setState({ searchField: e.target.value});
-  }
+  };
+
+
+  handleNewFormChange = evt => {
+    const newVideo = {...this.state.newVideo};
+    newVideo[evt.target.title] = evt.target.value;
+    this.setState({newVideo});
+  };
+
   
+  handleSubmit = evt => {
+    evt.preventDefault();
+    axios.post("/api/apivideo", this.state.newVideo).then(()=>{
+      this.setState({
+        newVideo: {
+          title:"",
+          link:"",
+          description: "",
+          topic:"",
+        }
+      });
+    });
+    this.updatePage()
+  }
   
   render() {
     const {apiVideos, searchField} = this.state;
@@ -39,7 +67,42 @@ export default class Apis extends Component {
             hanldeChange = {this.handleChange }
             />
         <VideoList apiVideos={filteredVideos}></VideoList>
-        
+     
+        <form onSubmit={this.handleSubmit} >
+          <label>New Video</label>
+          <br/>
+          <label>Title</label>
+          <input
+            type="text"
+            name="title"
+            onChange={this.handleNewFormChange}
+            //value= {this.state.newVideo.title}
+            />
+            <label>Description</label>
+            <input  
+              type="text"
+              name="description"
+              onChange={this.handleNewFormChange}
+              //value= {this.state.newVideo.description}
+              />
+              <label>Topic</label>
+              <input  
+              type="text"
+              name="topic"
+              onChange={this.handleNewFormChange}
+              //value= {this.state.newVideo.topic}
+              />
+              <label>Link</label>
+              <input  
+              type="text"
+              name="link"
+              onChange={this.handleNewFormChange}
+              //value= {this.state.newVideo.link}
+              />
+              <input type="submit" value="Add New Video" />
+        </form>
+      
+
       </div>
     )
   }
