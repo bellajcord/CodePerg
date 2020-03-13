744dfa88718f381653c2de22641ef7902b2302e7
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-03-13 09:09:54 -0400

    debugged props issue

--- client/src/components/Api/api-indiv-component.jsx
+++ client/src/components/Api/api-indiv-component.jsx
@@ -1,20 +1,22 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 //individual show page for each video will contiain edit and delete button
-import React from 'react'
-console.log(this.prop.apiVideo)
-export const IndivApiVideo = props => (
-//     <div className='indiv-video-container'>
-//  <iframe width="280" height="157" 
-//         src={`https://www.youtube.com/embed/${this.props.apiVideo.link} `}
-//         frameborder="0" 
-//         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-//         allowfullscreen></iframe>
-    
-//         <h2>{props.apiVideo.title} </h2>
-//         <p>{props.apiVideo.topic}</p>
-//         <p>{props.apiVideo.description}</p>
-        
-//     </div>
-<div>
-test</div>
-)
+import React from "react";
+// console.log(this.prop.apiVideo)
+export const IndivApiVideo = props => {
+  return (
+    <div className="indiv-video-container">
+      <iframe
+        width="280"
+        height="157"
+        src={`https://www.youtube.com/embed/${props.apiVideo.link} `}
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+
+      <h2>{props.apiVideo.title} </h2>
+      <p>{props.apiVideo.topic}</p>
+      <p>{props.apiVideo.description}</p>
+    </div>
+  );
+};
--- client/src/components/Api/api-list-component.jsx
+++ client/src/components/Api/api-list-component.jsx
@@ -1,16 +1,14 @@
 // all videos listed and add-to button
 
-import React from 'react';
-import {IndivApiVideo} from './api-indiv-component';
+import React from "react";
+import { IndivApiVideo } from "./api-indiv-component";
 
-export const VideoList = props => (
-    <div className='video-list'>
-    
-     {
-         props.apiVideos.map(apiVideo => (
-           <IndivApiVideo key={apiVideo._id} apiVideo={apiVideo} />
-          ))
-        }
+export const VideoList = props => {
+  return (
+    <div className="video-list">
+      {props.apiVideos.map(apiVideo => (
+        <IndivApiVideo key={apiVideo._id} apiVideo={apiVideo} />
+      ))}
     </div>
-
-)    
+  );
+};
