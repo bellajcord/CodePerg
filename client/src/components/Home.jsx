89788de56c91bd2c13579fc21ef94b4366c2cb5b
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-03-13 19:20:38 -0400

    npm install bootstrap

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,35 @@
 // will contain nav bar, and level selector buttons that route to subjects
 import React, { Component } from "react";
+import '../assets/bootstrap/css/bootstrap.min.css'
 
 export default class Home extends Component {
   render() {
     return (
       <div>
+      <nav id="mainNav" className="navbar navbar-dark navbar-expand-lg fixed-top bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="/">codePerg</a>
+          <button className="navbar-toggler navbar-toggler-right" data-target="#navbarResponsive" data-toggle="collapse" type="button" data-toggle="collapse" aria-controls="false" aria-label="Toggle navigation" >
+            <i className="fa fa-bars"></i>
+          </button>
+          <div id="navbarResponsive" className="collapse navbar-collapse">
+            <ul className="nav navbar-nav ml-auto text-uppercase">
+              <li className="nav-item" role="presentation">
+                <a className="nav-link js-scroll-trigger" href="/Libraries">Libraries</a>
+              </li>
+              <li className="nav-item" role="presentation">
+                <a className="nav-link js-scroll-trigger" href="/Languages">Languages</a>
+              </li>
+              <li className="nav-item" role="presentation">
+                <a className="nav-link js-scroll-trigger" href="/Apis">API's</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      
+    
         <div className="home-container">
           <div>
             <h1 className="home-header">Code Pergatory</h1>
