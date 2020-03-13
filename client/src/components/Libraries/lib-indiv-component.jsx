4a5735952edc8233222beed3dbb495f6a0d58c55
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-03-13 11:04:16 -0400

    debugged iframe in lib indiv

--- client/src/components/Libraries/lib-indiv-component.jsx
+++ client/src/components/Libraries/lib-indiv-component.jsx
@@ -1,18 +1,22 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 //individual show page for each video will contiain edit and delete button
-import React from 'react'
+import React from "react";
 
-export const IndivLibVideo = props => (
-    <div className='indiv-video-container'>
-    <iframe width="560" height="315" 
-        src="https://www.youtube.com/embed/?{libVideo.link}" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-        allowfullscreen></iframe>
-    
-        <h2>{props.libVideo.title} </h2>
-        <p>{props.libVideo.library}</p>
-        <p>{props.libVideo.description}</p>
-        
-    </div>
+export const IndivLibVideo = props => {
+    return(
+  <div className="indiv-video-container">
+    <iframe
+      width="280"
+      height="157"
+      src={`https://www.youtube.com/embed/${props.libVideo.link} `}
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen="allowfullscreen"
+    ></iframe>
 
-)
+    <h2>{props.libVideo.title} </h2>
+    <p>{props.libVideo.library}</p>
+    <p>{props.libVideo.description}</p>
+  </div>
+);
+    };
