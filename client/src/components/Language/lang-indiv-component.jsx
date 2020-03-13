4cc09187e3bf2dbaf7092d985360cdc2869a6987
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-03-13 10:28:21 -0400

    revised iframe in lang indiv

--- client/src/components/Language/lang-indiv-component.jsx
+++ client/src/components/Language/lang-indiv-component.jsx
@@ -1,17 +1,21 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 //individual show page for each video will contiain edit and delete button
 import React from 'react'
 
-export const IndivLangVideo = props => (
+export const IndivLangVideo = props => {
+    return (
     <div className='indiv-video-container'>
-    <iframe width="560" height="315" 
-        src="https://www.youtube.com/embed/?{langVideo.link}" 
+    <iframe width="280" height="157" 
+        src={'https://www.youtube.com/embed/${props.langVideo.link}'} 
         frameborder="0" 
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-        allowfullscreen></iframe>
+        allowfullscreen="allowfullscreen">
+        </iframe>
     
         <h2>{props.langVideo.name} </h2>
         <p>{props.langVideo.description}</p>
         <p>{props.langVideo.language}</p>
     </div>
 
-)
+);
+}
