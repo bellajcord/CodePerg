361b081ecc83fa99447fa040568464f55d20be77
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-03-13 10:58:59 -0400

    revised lib list components return

--- client/src/components/Libraries/lib-list-component.jsx
+++ client/src/components/Libraries/lib-list-component.jsx
@@ -1,16 +1,14 @@
 // all videos listed and add-to button
 
-import React from 'react';
-import {IndivLibVideo} from './lib-indiv-component';
+import React from "react";
+import { IndivLibVideo } from "./lib-indiv-component";
 
-export const VideoList = props => (
-    <div className='video-list'>
-    
-     {
-         props.libVideos.map(libVideo => (
-           <IndivLibVideo key={libVideo._id} libVideo={libVideo} />
-          ))
-        }
+export const VideoList = props => {
+  return (
+    <div className="video-list">
+      {props.libVideos.map(libVideo => (
+        <IndivLibVideo key={libVideo._id} libVideo={libVideo} />
+      ))}
     </div>
-
-)    
+  );
+};
