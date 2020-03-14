a617a63ff0be3cdf9fc4638d45ab32899f479039
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-03-14 13:18:35 -0400

    added styling to lib page

--- client/src/components/Apis.jsx
+++ client/src/components/Apis.jsx
@@ -1,165 +1,177 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import {VideoList} from './Api/api-list-component';
-import {SearchBox} from './Search-box';
-
+import React, { Component } from "react";
+import axios from "axios";
+import { VideoList } from "./Api/api-list-component";
+import { SearchBox } from "./Search-box";
 
 export default class Apis extends Component {
-  
-
-    state = {
-      apiVideos: [],
-      searchField: '',
-      apiVideo: {
-        title: "",
-        link: "",
-        description: "",
-        topic: "",
-      },
-      newVideo: {
-        title: "",
-        link: "",
-        description: "",
-        topic: "",
-      }
-      
-    };
-  
+  state = {
+    apiVideos: [],
+    searchField: "",
+    apiVideo: {
+      title: "",
+      link: "",
+      description: "",
+      topic: ""
+    },
+    newVideo: {
+      title: "",
+      link: "",
+      description: "",
+      topic: ""
+    }
+  };
 
   componentDidMount = () => {
-    axios.get('/api/apivideo').then(res => {
-        this.setState({apiVideos: res.data})
-    })
-  }
+    axios.get("/api/apivideo").then(res => {
+      this.setState({ apiVideos: res.data });
+    });
+  };
 
-  
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
 
- handleChange = (e) => {
-   this.setState({ searchField: e.target.value});
- };
-
-
-  handleNewFormChange = (event) => {
-    const attribute = event.target.name
-    const newVideo = { ...this.state.newVideo }
-    newVideo[attribute] = event.target.value
-    this.setState({ newVideo: newVideo })
-  }
+  handleNewFormChange = event => {
+    const attribute = event.target.name;
+    const newVideo = { ...this.state.newVideo };
+    newVideo[attribute] = event.target.value;
+    this.setState({ newVideo: newVideo });
+  };
 
   //form
   handleSubmit = evt => {
     evt.preventDefault();
-    axios.post("/api/apivideo", this.state.newVideo).then(()=>{
+    axios.post("/api/apivideo", this.state.newVideo).then(() => {
       this.setState({
         newVideo: {
-          title:"",
-          link:"",
+          title: "",
+          link: "",
           description: "",
-          topic:"",
+          topic: ""
         }
       });
     });
-    this.componentDidMount()
-    
-  }
-  
+    this.componentDidMount();
+  };
+
   render() {
-    const {apiVideos, searchField} = this.state;
-    const filteredVideos = apiVideos.filter( apiVideo => 
+    const { apiVideos, searchField } = this.state;
+    const filteredVideos = apiVideos.filter(apiVideo =>
       apiVideo.title.toLowerCase().includes(searchField.toLowerCase())
-      );
+    );
 
     return (
       <div>
-
-      <nav id="mainNav" className="navbar navbar-dark navbar-expand-lg fixed-top bg-dark">
-        <div className="container">
-          <a className="navbar-brand" href="/">codePerg</a>
-          <button className="navbar-toggler navbar-toggler-right" data-target="#navbarResponsive" data-toggle="collapse" type="button" data-toggle="collapse" aria-controls="false" aria-label="Toggle navigation" >
-            <i className="fa fa-bars"></i>
-          </button>
-          <div id="navbarResponsive" className="collapse navbar-collapse">
-            <ul className="nav navbar-nav ml-auto text-uppercase">
-              <li className="nav-item" role="presentation">
-                <a className="nav-link js-scroll-trigger" href="/Libraries">Libraries</a>
-              </li>
-              <li className="nav-item" role="presentation">
-                <a className="nav-link js-scroll-trigger" href="/Languages">Languages</a>
-              </li>
-              <li className="nav-item" role="presentation">
-                <a className="nav-link js-scroll-trigger" href="/Apis">API's</a>
-              </li>
-            </ul>
+        <nav
+          id="mainNav"
+          className="navbar navbar-dark navbar-expand-lg fixed-top bg-dark"
+        >
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              codePerg
+            </a>
+            <button
+              className="navbar-toggler navbar-toggler-right"
+              data-target="#navbarResponsive"
+              data-toggle="collapse"
+              type="button"
+              data-toggle="collapse"
+              aria-controls="false"
+              aria-label="Toggle navigation"
+            >
+              <i className="fa fa-bars"></i>
+            </button>
+            <div id="navbarResponsive" className="collapse navbar-collapse">
+              <ul className="nav navbar-nav ml-auto text-uppercase">
+                <li className="nav-item" role="presentation">
+                  <a className="nav-link js-scroll-trigger" href="/Libraries">
+                    Libraries
+                  </a>
+                </li>
+                <li className="nav-item" role="presentation">
+                  <a className="nav-link js-scroll-trigger" href="/Languages">
+                    Languages
+                  </a>
+                </li>
+                <li className="nav-item" role="presentation">
+                  <a className="nav-link js-scroll-trigger" href="/Apis">
+                    API's
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
 
-      <div className='video-display' >
-      <h1>Api Videos</h1>
-      <br/>
-      <SearchBox 
+        <div className="video-display">
+          <h1>Api Videos</h1>
+          <br />
+          <SearchBox
             placeholder="search videos"
-            hanldeChange = {this.handleChange }
-            />
-      <br/>
-      <hr/>
-      <br/>
-        <VideoList apiVideos={filteredVideos}></VideoList>
-     <section className="add-new-form">
-         <form onSubmit={this.handleSubmit} >
-           <label className='newform-label'>New Video</label>
-           <br/>
-           <label>Title</label>
-           <input
-             type="text"
-             name="title"
-             onChange={this.handleNewFormChange}
-             //value= {this.state.newVideo.title}
-             />
-             <br/>
-             <label>Description</label>
-             <input  
-               type="text"
-               name="description"
-               onChange={this.handleNewFormChange}
-               //value= {this.state.newVideo.description}
-               />
-               <br/>
-               <label>Topic</label>
-               <input  
-               type="text"
-               name="topic"
-               onChange={this.handleNewFormChange}
-               //value= {this.state.newVideo.topic}
-               />
-               <br/>
-               <label>Link</label>
-               <input  
-               type="text"
-               name="link"
-               onChange={this.handleNewFormChange}
-               //value= {this.state.newVideo.link}
+            hanldeChange={this.handleChange}
+          />
+          <br />
+          <hr />
+          <br />
+          <VideoList apiVideos={filteredVideos}></VideoList>
+          <section className="add-new-form">
+            <form onSubmit={this.handleSubmit}>
+              <label className="newform-label">New Video</label>
+              <br />
+              <label>Title</label>
+              <input
+                type="text"
+                name="title"
+                onChange={this.handleNewFormChange}
+                //value= {this.state.newVideo.title}
               />
-              <br/>
-              <br/>
-              <input className="new-video-button" type="submit" value="Add New Video" />
-         </form>
-      </section>
+              <br />
+              <label>Description</label>
+              <input
+                type="text"
+                name="description"
+                onChange={this.handleNewFormChange}
+                //value= {this.state.newVideo.description}
+              />
+              <br />
+              <label>Topic</label>
+              <input
+                type="text"
+                name="topic"
+                onChange={this.handleNewFormChange}
+                //value= {this.state.newVideo.topic}
+              />
+              <br />
+              <label>Link</label>
+              <input
+                type="text"
+                name="link"
+                onChange={this.handleNewFormChange}
+                //value= {this.state.newVideo.link}
+              />
+              <br />
+              <br />
+              <input
+                className="new-video-button"
+                type="submit"
+                value="Add New Video"
+              />
+            </form>
+          </section>
 
-      <footer>
-      <section className="secondary-footer">
-      <div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          <span className="copyright">Copyright© codePerg 2020</span>
+          <footer>
+            <section className="secondary-footer">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-4">
+                    <span className="copyright">Copyright© codePerg 2020</span>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </footer>
         </div>
       </div>
-    </div>
-    </section>
-      </footer>
-
-      </div>
-      </div>
-    )
+    );
   }
 }
--- client/src/components/Libraries.jsx
+++ client/src/components/Libraries.jsx
@@ -1,124 +1,185 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import {VideoList} from './Libraries/lib-list-component'
-import {SearchBox} from './Search-box'
-
+import React, { Component } from "react";
+import axios from "axios";
+import { VideoList } from "./Libraries/lib-list-component";
+import { SearchBox } from "./Search-box";
 
 export default class Libraries extends Component {
-  
-state = {
-      libVideos: [],
-      searchField: '',
-      libVideo: {
-        title: '',
-        link: '',
-        description: '',
-        library:'',
-      },
-      newVideo: {
-        title: '',
-        link:'',
-        description:'',
-        library:'',
-      }
-    };
-  
+  state = {
+    libVideos: [],
+    searchField: "",
+    libVideo: {
+      title: "",
+      link: "",
+      description: "",
+      library: ""
+    },
+    newVideo: {
+      title: "",
+      link: "",
+      description: "",
+      library: ""
+    }
+  };
 
   componentDidMount = () => {
-    axios.get('/api/libraries').then(res => {
-        this.setState({libVideos: res.data})
-    })
-  }
+    axios.get("/api/libraries").then(res => {
+      this.setState({ libVideos: res.data });
+    });
+  };
 
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value});
-  }
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
 
-  handleNewFormChange = (event) => {
-    const attribute = event.target.name
-    const newVideo = { ...this.state.newVideo }
-    newVideo[attribute] = event.target.value
-    this.setState({ newVideo: newVideo })
-  }
-  
+  handleNewFormChange = event => {
+    const attribute = event.target.name;
+    const newVideo = { ...this.state.newVideo };
+    newVideo[attribute] = event.target.value;
+    this.setState({ newVideo: newVideo });
+  };
+
   handleSubmit = evt => {
     evt.preventDefault();
-    axios.post("/api/libraries", this.state.newVideo).then(()=>{
+    axios.post("/api/libraries", this.state.newVideo).then(() => {
       this.setState({
         newVideo: {
-          title:"",
-          link:"",
+          title: "",
+          link: "",
           description: "",
-          topic:"",
+          topic: ""
         }
       });
     });
-    this.componentDidMount()
-    
-  }
+    this.componentDidMount();
+  };
 
   deleteButtonAction = () => {
-    axios
-      .delete(
-        `/api/libraries/${this.state.match.params.libVideoId}`,
-        this.state.libVideos
-      )
-      
+    axios.delete(
+      `/api/libraries/${this.state.match.params.libVideoId}`,
+      this.state.libVideos
+    );
+
     this.componentDidMount();
-      
   };
-  
+
   render() {
-    const {libVideos, searchField} = this.state;
-    const filteredVideos = libVideos.filter( libVideo => 
+    const { libVideos, searchField } = this.state;
+    const filteredVideos = libVideos.filter(libVideo =>
       libVideo.title.toLowerCase().includes(searchField.toLowerCase())
-      );
+    );
 
     return (
-      <div className='video-display' >
-      <h1>Library Videos</h1>
-      <SearchBox 
-            placeholder="search videos"
-            hanldeChange = {this.handleChange }
-            />
-        <VideoList libVideos={filteredVideos}></VideoList>
-        
-        <form onSubmit={this.handleSubmit} >
-        <label>New Video</label>
-        <br/>
-        <label>Title</label>
-        <input
-          type="text"
-          name="title"
-          onChange={this.handleNewFormChange}
-          //value= {this.state.newVideo.title}
-          />
-          <label>Description</label>
-          <input  
-            type="text"
-            name="description"
-            onChange={this.handleNewFormChange}
-            //value= {this.state.newVideo.description}
-            />
-            <label>Library</label>
-            <input  
-            type="text"
-            name="library"
-            onChange={this.handleNewFormChange}
-            //value= {this.state.newVideo.topic}
-            />
-            <label>Link</label>
-            <input  
-            type="text"
-            name="link"
-            onChange={this.handleNewFormChange}
-            //value= {this.state.newVideo.link}
-           />
-           <input type="submit" value="Add New Video" />
-      </form>
-   
+      <div>
+        <nav
+          id="mainNav"
+          className="navbar navbar-dark navbar-expand-lg fixed-top bg-dark"
+        >
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              codePerg
+            </a>
+            <button
+              className="navbar-toggler navbar-toggler-right"
+              data-target="#navbarResponsive"
+              data-toggle="collapse"
+              type="button"
+              data-toggle="collapse"
+              aria-controls="false"
+              aria-label="Toggle navigation"
+            >
+              <i className="fa fa-bars"></i>
+            </button>
+            <div id="navbarResponsive" className="collapse navbar-collapse">
+              <ul className="nav navbar-nav ml-auto text-uppercase">
+                <li className="nav-item" role="presentation">
+                  <a className="nav-link js-scroll-trigger" href="/Libraries">
+                    Libraries
+                  </a>
+                </li>
+                <li className="nav-item" role="presentation">
+                  <a className="nav-link js-scroll-trigger" href="/Languages">
+                    Languages
+                  </a>
+                </li>
+                <li className="nav-item" role="presentation">
+                  <a className="nav-link js-scroll-trigger" href="/Apis">
+                    API's
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
 
+        <div className="video-display">
+          <h1>Library Videos</h1>
+          <br />
+          <SearchBox
+            placeholder="search videos"
+            hanldeChange={this.handleChange}
+          />
+          <br />
+          <hr />
+          <br />
+          <VideoList libVideos={filteredVideos}></VideoList>
+          <section className="add-new-form">
+            <form onSubmit={this.handleSubmit}>
+              <label className="newform-label">New Video</label>
+              <br />
+              <label>Title</label>
+              <input
+                type="text"
+                name="title"
+                onChange={this.handleNewFormChange}
+                //value= {this.state.newVideo.title}
+              />
+              <br />
+              <label>Description</label>
+              <input
+                type="text"
+                name="description"
+                onChange={this.handleNewFormChange}
+                //value= {this.state.newVideo.description}
+              />
+              <br />
+              <label>Library</label>
+              <input
+                type="text"
+                name="library"
+                onChange={this.handleNewFormChange}
+                //value= {this.state.newVideo.topic}
+              />
+              <br />
+              <label>Link</label>
+              <input
+                type="text"
+                name="link"
+                onChange={this.handleNewFormChange}
+                //value= {this.state.newVideo.link}
+              />
+              <br />
+              <br />
+              <input
+                className="new-video-button"
+                type="submit"
+                value="Add New Video"
+              />
+            </form>
+          </section>
+
+          <footer>
+            <section className="secondary-footer">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-4">
+                    <span className="copyright">Copyright© codePerg 2020</span>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </footer>
+        </div>
       </div>
-    )
+    );
   }
 }
